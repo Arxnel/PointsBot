eb10406e4b13e124739bfc908f68b1d7660bcab5
DuckedEnough <64556063+Arxnel@users.noreply.github.com>
2023-06-13 03:28:04 +0000

    ddd

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 Token = os.getenv('TOKEN')
-Prefix = os.getenv('PREFIX', '.')  # The second argument is a default value in case the environment variable is not set
-PointsName = os.getenv('POINTS_NAME', 'Goon Tokens')
-Log_channel = int(os.getenv('LOG_CHANNEL', '821442828056002602'))
-POINTS_TO_BE_GIVEN_PER_MESSAGE = int(os.getenv('POINTS_PER_MESSAGE', '2'))
+Prefix = os.getenv('PREFIX')  # The second argument is a default value in case the environment variable is not set
+PointsName = os.getenv('POINTS_NAME',)
+Log_channel = int(os.getenv('LOG_CHANNEL'))
+POINTS_TO_BE_GIVEN_PER_MESSAGE = int(os.getenv('POINTS_PER_MESSAGE',))
