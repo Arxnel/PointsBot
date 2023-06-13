9bd4bf5294c9f709ed241dd340593d6bba9b767e
DuckedEnough <64556063+Arxnel@users.noreply.github.com>
2023-06-13 03:21:17 +0000

    ddd

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-Token = "TOKEN"
-Prefix = '.'
-PointsName = "Goon Tokens"
-Log_channel = 821442828056002602 # Channel where all shop purchases are logged.
-POINTS_TO_BE_GIVEN_PER_MESSAGE = 2 # Points which will be given to members per message.
+import os
+
+Token = os.getenv('TOKEN')
+Prefix = os.getenv('PREFIX', '.')  # The second argument is a default value in case the environment variable is not set
+PointsName = os.getenv('POINTS_NAME', 'Goon Tokens')
+Log_channel = int(os.getenv('LOG_CHANNEL', '821442828056002602'))
+POINTS_TO_BE_GIVEN_PER_MESSAGE = int(os.getenv('POINTS_PER_MESSAGE', '2'))
